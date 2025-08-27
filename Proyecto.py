0ab0a8fce4152f0f3d5f5096711b5dc49c48c210
AlfonsoCBTEC <A01714310@tec.mx>
2025-08-26 18:40:35 -0600

    Update Proyecto.py
    I Changeg the Language to English
    I added a better description of how the program works
    I started to build the menu

--- Proyecto.py
+++ Proyecto.py
@@ -1,8 +1,27 @@
 """
-Proyecto Demo python
-Gestor de finanzas
-El programa permite al usuario registrar sus ingresos y gastos. 
-Muestra al usuario el balance de cuenta, promedio de gastos 
-semanales/diarios, gatsos maximos y minimos al igual que el porcentaje 
-de ingresos gastados al mes
+
+Demo python proyect by Alfonso Carranza Bassoco - A01714310
+Finance manager
+The program allow the user to register its income and expenses.
+It shows the user the balance of account and also certain statistics
+such as:
+  -Maximun and minimun expenses
+  -Percentage of income spent
+  -Average expenses
+It also allow the user to view the expenses by category (which are specified
+when the user register its expenses)
+The program allows the user to search, delete and edit any movement.
 """
+
+
+
+"""
+========  Menu of the program ========================================
+"""
+
+
+while True:
+
+  option = input("Select an option:\n 1---Register income \n 2---Register expense \n 3---Show balance of account \n\
+                  4---Avergae of expenses \n 
+
