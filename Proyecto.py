b91d6888af3536fe9d2a20e6265af1c9cd03f0c7
AlfonsoCBTEC <A01714310@tec.mx>
2025-08-26 18:16:05 -0600

    Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,8 @@
+"""
+Proyecto Demo python
+Gestor de finanzas
+El programa permite al usuario registrar sus ingresos y gastos. 
+Muestra al usuario el balance de cuenta, promedio de gastos 
+semanales/diarios, gatsos maximos y minimos al igual que el porcentaje 
+de ingresos gastados al mes
+"""
