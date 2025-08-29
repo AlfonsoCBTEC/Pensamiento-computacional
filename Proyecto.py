4c2c45228528f71aa25a7cb836c78c2836887528
AlfonsoCBTEC <A01714310@tec.mx>
2025-08-29 08:14:09 -0600

    Update 3 Proyecto.py
    -Created history dictionary
    -Defined the "resgister income" function

--- Proyecto.py
+++ Proyecto.py
@@ -18,9 +18,13 @@ import msvcrt #Allows me to enter any button to continue
 
 
 """
-================== Welcome text  =====================================
+================== Welcome text and defining dictionarys  =====================================
 """
-print("Welcome to Alfonso's Finance Manager")
+print("Welcome to Alfonso's Finance Manager\n")
+
+history = {
+}
+
 
 """
 ================== Functions  =====================================
@@ -30,6 +34,47 @@ def clear_screen():
   os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def register_income():
+  try:
+
+    inc_amount = int(input("Register the amount: "))
+
+    inc_description = input("Register a description: ")
+
+    inc_year = int(input("Register a year: "))
+    if 2000 > inc_year or inc_year > 2100:
+      print("Please select a valid year")
+      return
+    
+    if inc_year not in history:
+      history[inc_year]= {}
+
+    inc_month = int(input("Register Month (1-12): "))
+    if 0 >= inc_month  or inc_month> 12:
+      print("Please select a valid month")
+      return
+    
+    if inc_month not in history[inc_year]:
+      history[inc_year][inc_month] = []
+    
+    # Once the program validated the inputs and added new years/months
+    # the movement is created and added to "history"
+  
+    movement = {"type": "Income", "amount": inc_amount, "description": inc_description}
+
+    history[inc_year][inc_month].append(movement)
+
+    print("\nYour movement has been added succesfully\n")
+
+  except ValueError:
+    print("Insert valid data")
+    return
+  
+def register_expense():
+  
+
+
+
 
 """
 ========  Menu of the program ========================================
@@ -37,11 +82,19 @@ def clear_screen():
 
 while True:
   print("Click to continue")
-  clear_screen()
   msvcrt.getch()
-  option = input("Select an option:\n 1---Register income \n 2---Register expense \n 3---Show balance of account \n\
-  4---View statistics \n 5---View expenses by category \n 6---Search/edit/delete movements \n\
-  7---Load data \n 8---Exit")
+  clear_screen()
+  option = input(
+    "Select an option:\n"
+    "1---Register income\n" 
+    "2---Register expense\n"
+    "3---Show balance of account\n"
+    "4---View statistics\n" 
+    "5---View expenses by category\n" 
+    "6---Search/edit/delete movements\n"
+    "7---Load data\n" 
+    "8---Exit\n"
+  )
   
   if option == "1":
     register_income()
